e4551548a5e60ff77fe167608575e5bf70a98cd8
Sajjadul <sbappyi200@gmail.com>
2021-02-07 20:35:32 +0600

    add functionality

--- script.js
+++ script.js
@@ -10,9 +10,66 @@ function getData(mealName){
     .then(data => displayData(data.meals))
     .catch(error => console.log(error))
 }
-//getData();
+
 
 function displayData(data){
+    console.log(data)
     const mealContainer = document.getElementById('meal-container');
+    data.forEach(element => {
+        const div = document.createElement('div');
+        div.className = 'meal';
+        
+        const img = document.createElement('img')
+        img.src = element.strMealThumb;
+
+        const h3 = document.createElement('h3');
+        h3.innerText = element.strMeal;
+
+        div.appendChild(img)
+        div.appendChild(h3)
+
+        div.addEventListener('click',()=>mealData(element.idMeal))
+
+        mealContainer.appendChild(div);
+
+    });
+}
+
+
+// get single meal data 
+function mealData(idMeal){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`)
+    .then(response => response.json())
+    .then(data => displayMealData(data.meals[0]))
+    .catch(error => console.log(error))
+}
+
+
+// display single meal data
+function displayMealData(data){
+    console.log(data)
+    const mealContainer = document.getElementById('meal-container');
+    mealContainer.style.display = 'none';
+
+    const mealDetails = document.getElementById('meal-details');
+    mealDetails.style.display = 'block';
+
+    const exitBtn = document.getElementById('exit-btn');
+    exitBtn.style.fontSize='20px'
+    exitBtn.addEventListener('click',()=>{
+        mealDetails.style.display = 'none';
+        mealContainer.style.display = 'grid';
+
+    })
+    const img = document.getElementById('meal-image');
+    img.src = data.strMealThumb;
+
+    document.getElementById('item1').innerText =  data.strMeasure1;
+    document.getElementById('item2').innerText =  data.strMeasure2;
+    document.getElementById('item3').innerText =  data.strMeasure3;
+    document.getElementById('item4').innerText =  data.strMeasure4;
+  
+
+  
     
 }
